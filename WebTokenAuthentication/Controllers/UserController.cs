03d39150e0c08d090e68c70533e4dd9d4786f868
Anshu Kumari <anshukumari904@gmail.com>
2021-09-25 15:12:00 +0530

    Token Based Authentication

---
+++ WebTokenAuthentication/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace WebTokenAuthentication.Controllers
+{
+    public class UserController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/data/forall")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Now server time is:"+DateTime.Now.ToString());
+        }
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/authenticate")]
+        public IHttpActionResult GetForAuthenticate()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok("Hello "+identity.Name);
+        }
+        [Authorize]
+        [HttpGet]
+        [Route("api/data/admin")]
+        public IHttpActionResult GetForAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims
+                        .Where(c => c.Type == ClaimTypes.Role)
+                        .Select(c => c.Value);
+            return Ok("Hello " + identity.Name + " Role: " + string.Join(",", roles.ToList()));
+        }
+    }
+}
